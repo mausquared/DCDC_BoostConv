948dc51c74f26ec7602769403c16513faf493c3b
mausquared <51234688+mausquared@users.noreply.github.com>
2022-05-15 16:59:36 +0200

    Add files via upload
    Tweaked parameters more.

--- calculation_coil.m
+++ calculation_coil.m
@@ -1,28 +1,37 @@
-%% Calculation of inductance
-Vd = 30;
-D = 0.5;
-Fs = 10000;
-Delta_il = 8.6;
-L1 = (Vd*D) / (Fs * Delta_il);
-disp(L1)
-%% Calculation of coil 
-% maximum L = 3.75e-4, Lmin = 1.74e-4
-u_r = 1560;
-turns = 14;
-area = 58e-6;
-length = 0.057;
-u = 1.26e-6;
+clc
+clear
+%% calcultion design example
+Vin = 30;
+Vo = 60;
+Fsw = 40000;
+Ro = 10;
+delta_vo = 0.04*Vo; 
 
-L_coil = (u_r * turns^2 * area * u) / length
+D = 1 - (Vin / Vo);
+%I_L = Vo^2 / (Ro*Vin); % note that I_L is the same as I_in
+delta_il = 0.15*5; % ripple in inductor is limited to 20% of average inductor current
+L = (Vin*D) / (delta_il * Fsw); % inductor value 
+C = (Vin*D) / (delta_vo*(1-D)*Ro*Fsw);
+
+disp(L)
+%% Calculation of coil 
+u_r = 246; %this is for gap equal to 0.27mm
+turns = 40; % checked for multiple values of N, this should be correct 
+area = 6.9e-5; %Ae = 69mm^2
+length = 0.068; %length = 68mm
+u = 1.26e-6; % this is constant 
+
+L_coil = (u_r * turns^2 * area * u) / length;
 disp(L_coil)
+
 %% Calculation for wire thickness
 % use EFD 25/13/9 magnetics circuit 
-D = area / pi;
+Diameter = area / pi;
 % Use AWG 17 of copper table 
 T = 0.124;
 
 
-Wl = pi*(D+T/2)*(length/T)*turns;
+Wl = pi*(Diameter+T/2)*(length/T)*turns;
 disp(Wl)
 
 
